db38c49e33ba029c7c822ebbb45509b5ac3bf1af
Marina <marina.zilkina@gmail.com>
2023-06-30 17:16:55 +0300

    finished

--- backend_api/core/views.py
+++ backend_api/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK
 
 from .models import Todo
 from .serializers import TodoSerializer
@@ -28,3 +28,25 @@ def todos_list(request):
         serializer = TodoSerializer(todo, many=False)
         return Response(serializer.data)
 
+
+@api_view(['GET', 'DELETE', 'PATCH']) # здесь определяем каким методы будет использлвать
+def todo(request, pk):
+    if request.method == 'GET':
+        todo = Todo.objects.get(pk=pk)
+        # передаем данные для сериализации в сериалайзер, даем знать, что обьектов будет много (many)
+        serializer = TodoSerializer(todo, many=False)
+        # возвращаем обработанные данные
+        return Response(serializer.data)
+
+    if request.method == 'DELETE':
+        todo = Todo.objects.get(pk=pk)
+        todo.delete()
+        return Response(status=HTTP_200_OK)
+
+    if request.method == 'PATCH':
+        todo = Todo.objects.get(pk=pk)
+        todo.name = request.data.get('name')
+        todo.status = request.data.get('status')
+        todo.save()
+        serializer = TodoSerializer(todo, many=False)
+        return Response(serializer.data)
